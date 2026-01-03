d2eeda5ceb598d99f552aec1c60619da5c28b7b5
David <emekachinye2020@gmail.com>
2026-01-03 20:29:39 +0100

    built landing page sections

--- components/CountDownTimer.tsx
+++ components/CountDownTimer.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import { ChevronRight } from "lucide-react";
 
 type Props = {
   targetDate: string;
@@ -18,26 +20,29 @@ export default function EventStrip({ targetDate }: Props) {
   }, [targetDate]);
 
   return (
-    <section className="w-full bg-[#1E2F5C]">
-      <div className="mx-auto flex max-w-7xl items-center justify-between px-9 py-6 text-white">
+    <section className="w-full bg-[#1E2F5C] py-5 px-6">
+      <div className="mx-auto flex flex-col md:flex-row max-w-7xl items-center justify-between px-9 py-6 text-white">
         {/* Left */}
-        <div>
-          <div className="mb-1 flex items-center gap-2 text-[10px] uppercase tracking-wide text-white/70">
-            <span className="h-3 w-[2px] bg-white" />
+        <div className="items-left md:items-center mb-4">
+          <div className="mb-1 flex items-center gap-2 text-[14px] uppercase tracking-wide text-[#C3A253]">
+      
+            <Image src={"/images/bar-icon.png"} width={10} height={10} alt="about image"/>
             Online
           </div>
-          <p className="text-sm font-semibold">Healing Stream</p>
-          <p className="text-xs text-white/60">
+          <p className="text-md font-semibold">Healing Stream</p>
+          <p className="text-sm text-white/60">
             December 16, 2025 @ 5:00 am
           </p>
+           <button className="mt-4 flex items-center gap-2 text-sm font-medium text-white cursor-pointer">
+              Join Now
+              <span className="inline-flex h-5 w-5 items-center justify-center rounded-full border border-white">
+                <ChevronRight />
+              </span>
+            </button>
         </div>
 
         {/* Right Countdown */}
-        <div
-          className="flex items-center gap-8"
-          role="timer"
-          aria-live="polite"
-        >
+        <div className="flex flex-row items-center gap-8 px-5" role="timer" aria-live="polite">
           <Time value={time.days} label="Days" />
           <Time value={time.hours} label="Hours" />
           <Time value={time.minutes} label="Minutes" />
@@ -51,10 +56,10 @@ export default function EventStrip({ targetDate }: Props) {
 function Time({ value, label }: { value: number; label: string }) {
   return (
     <div className="text-center">
-      <div className="text-lg font-bold tabular-nums">
+      <div className="text-xl font-bold tabular-nums p-1.5">
         {String(value).padStart(2, "0")}
       </div>
-      <div className="text-[10px] uppercase text-white/60">
+      <div className="text-[10px] font-semibold uppercase text-[#C3A253] bg-gray-500/50 p-2">
         {label}
       </div>
     </div>
